53c1d37dad01fa18cb953c5d622c172aac635baa
Imrager <davidlclarke92@yahoo.com>
2019-11-14 17:47:59 -0500

    added styling and added weather api call

--- src/App.js
+++ src/App.js
@@ -8,19 +8,53 @@ class App extends Component {
   constructor(props){
   super(props)
   this.state = {
-    city: '',
-    temparature: ''
+    city: [],
+    query: '',
   }
 }
-componentDidMount(){
 
-}
+
+// gets city based off of search
+   getCity = () => {fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&APPID=4c58ee73def863ccc33defea4c884388`)
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ city: data })
+        })
+        .catch(console.log)
+      }
+      
+      // getInfo = () => {
+      //   axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
+      //     .then(({ data }) => {
+      //       this.setState({
+      //         results: data.data
+      //       })
+      //     })
+      // }
+      handleInputChange = () => {
+        this.setState({
+          query: this.search.value
+        }, () => {
+          if (this.state.query && this.state.query.length > 1) {
+            if (this.state.query.length % 2 === 0) {
+              this.getCity()
+            }
+          } 
+        })
+      }
+
   render (){
   return (
     <div id='body'>
-      <h1>Weather App</h1>
-      <input placeholder="City"/>
-      <button>Search</button>
+      <h1 id='appTitle'>Weather App</h1>
+      <form id='search'>
+       <input
+         placeholder="Search for City"
+         ref={input => this.search = input}
+         onChange={this.handleInputChange}
+       />
+       <p>{this.state.query}</p>
+     </form>
       <ul>
         <li><img src={ cloudy } height='100px'/>
         <h1>65</h1></li>
