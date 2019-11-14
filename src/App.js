95a5ce7e0165b33a346ecb205c0e569eefa6ac83
Imrager <davidlclarke92@yahoo.com>
2019-11-14 13:04:57 -0500

    created template for app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import cloudy from "./images/sun.png"
+import sun from "./images/cloudywsun.jpg"
 
-function App() {
+class App extends Component { 
+  constructor(props){
+  super(props)
+  this.state = {
+    city: '',
+    temparature: ''
+  }
+}
+componentDidMount(){
+
+}
+  render (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='body'>
+      <h1>Weather App</h1>
+      <input placeholder="City"/>
+      <button>Search</button>
+      <ul>
+        <li><img src={ cloudy } height='100px'/>
+        <h1>65</h1></li>
+        <li><img src={ sun } height='100px'/>
+        <h1>75</h1></li>
+        <li><img src={ sun } height='100px'/>
+        <h1>80</h1></li>
+        <li><img src={ sun } height='100px'/>
+        <h1>83</h1></li>
+        <li><img src={ sun } height='100px'/>
+        <h1>85</h1></li>
+      </ul>
     </div>
   );
+  }
 }
 
 export default App;
